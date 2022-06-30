6b01729470d6a520445dc9935b0d992bc2157b4d
3bmo3ty <mohamedabdalmoty3@gmail.com>
2022-06-30 04:58:59 +0200

    adminlogin commit

--- src/app/components/admin-login/admin-login.component.ts
+++ src/app/components/admin-login/admin-login.component.ts
@@ -2,17 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/Services/auth.service';
-import jwt_decode from "jwt-decode";
+import jwt_decode from 'jwt-decode';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-admin-login',
   templateUrl: './admin-login.component.html',
-  styleUrls: ['./admin-login.component.scss']
+  styleUrls: ['./admin-login.component.scss'],
 })
 export class AdminLoginComponent implements OnInit {
   isLoging = false;
-  constructor(private _AuthService:AuthService, private _Router:Router) { }
+  isRequired = false;
+  constructor(private _AuthService: AuthService, private _Router: Router) {}
 
   adminLoginForm = new FormGroup({
     AdminID: new FormControl(null, [Validators.required]),
@@ -20,24 +21,32 @@ export class AdminLoginComponent implements OnInit {
   });
 
   adminLogin(adminInfo: any) {
-    this.isLoging = true;
-    
-    this._AuthService.adminLogin(adminInfo.value.AdminID, adminInfo.value.AdminPassword).subscribe((Response) => {
-      if(Response.message == "success"){
-        this._AuthService.adminToken = Response.token;
-        this._AuthService.admin = jwt_decode(Response.token);
-        this._Router.navigate(['/admincontrol'])
-        this.isLoging = false;
-      }else{
-        console.log(Response.message);
-        Swal.fire(Response.message)
-        this.isLoging = false;
-      }
-    });
+    if (adminInfo.value.AdminID != '' || adminInfo.value.AdminPassword) {
+      this.isLoging = true;
+      this.isRequired = false
+      this._AuthService
+        .adminLogin(adminInfo.value.AdminID, adminInfo.value.AdminPassword)
+        .subscribe((Response) => {
+          if (Response.message == 'success') {
+            this._AuthService.adminToken = Response.token;
+            this._AuthService.admin = jwt_decode(Response.token);
+            this._Router.navigate(['/admincontrol']);
+            this.isLoging = false;
+          } else {
+            console.log(Response.message);
+            Swal.fire(Response.message);
+            this.isLoging = false;
+          }
+        });
+    }else{
+      this.isRequired = true
+    }
   }
-
 
   ngOnInit(): void {
+    this.adminLoginForm.setValue({
+      AdminID: '',
+      AdminPassword: '',
+    });
   }
-
 }
